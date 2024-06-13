1f87c7e88e8cdac8f417300ea6bf8c4bb955d147
Euge-Losada <eugi_loxi@hotmail.com>
2024-06-13 16:37:44 -0300

    tp3tests

--- src/main/java/ar/edu/utn/frbb/tup/service/CuentaService.java
+++ src/main/java/ar/edu/utn/frbb/tup/service/CuentaService.java
@@ -1,37 +1,43 @@
 package ar.edu.utn.frbb.tup.service;
 
 import ar.edu.utn.frbb.tup.model.Cuenta;
+import ar.edu.utn.frbb.tup.model.TipoCuenta;
 import ar.edu.utn.frbb.tup.model.exception.CuentaAlreadyExistsException;
 import ar.edu.utn.frbb.tup.model.exception.TipoCuentaAlreadyExistsException;
 import ar.edu.utn.frbb.tup.persistence.CuentaDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 @Component
 public class CuentaService {
-    CuentaDao cuentaDao = new CuentaDao();
+    @Autowired
+    private CuentaDao cuentaDao;
 
     @Autowired
-    ClienteService clienteService;
+    private ClienteService clienteService;
 
-    //Generar casos de test para darDeAltaCuenta
-    //    1 - cuenta existente
-    //    2 - cuenta no soportada
-    //    3 - cliente ya tiene cuenta de ese tipo
-    //    4 - cuenta creada exitosamente
     public void darDeAltaCuenta(Cuenta cuenta, long dniTitular) throws CuentaAlreadyExistsException, TipoCuentaAlreadyExistsException {
-        if(cuentaDao.find(cuenta.getNumeroCuenta()) != null) {
+        if (cuentaDao.find(cuenta.getNumeroCuenta()) != null) {
             throw new CuentaAlreadyExistsException("La cuenta " + cuenta.getNumeroCuenta() + " ya existe.");
         }
 
-        //Chequear cuentas soportadas por el banco CA$ CC$ CAU$S
-        // if (!tipoCuentaEstaSoportada(cuenta)) {...}
+        // Chequear cuentas soportadas por el banco
+        if (!tipoCuentaEstaSoportada(cuenta)) {
+            throw new IllegalArgumentException("Tipo de cuenta no soportado: " + cuenta.getTipoCuenta());
+        }
 
         clienteService.agregarCuenta(cuenta, dniTitular);
         cuentaDao.save(cuenta);
+    }
+
+    private static final Set<TipoCuenta> TIPOS_CUENTA_SOPORTADOS = new HashSet<>(Arrays.asList(TipoCuenta.values()));
+
+    private boolean tipoCuentaEstaSoportada(Cuenta cuenta) {
+        return TIPOS_CUENTA_SOPORTADOS.contains(cuenta.getTipoCuenta());
     }
 
     public Cuenta find(long id) {
